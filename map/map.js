c68a7013171bf6ba1d2e23b09e84d091dfe22739
evaizli <everrich2018@gmail.com>
2019-06-07 20:29:45 -0700

    update map styling

--- map/map.js
+++ map/map.js
@@ -10,7 +10,7 @@ function createMap() {
         element: document.getElementById("basic_choropleth"),
         responsive: true,
         height: offsetHeight,
-        width: offsetWidth*(.58),
+        width: offsetWidth*(.95),
         fills: {
             EXTREME: "#ff6666",
             HIGH:"#FFCCCC",

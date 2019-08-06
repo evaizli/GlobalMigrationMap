07af6f74541bc7e5b47cebdd21bb8c158a4704d6
evaizli <everrich2018@gmail.com>
2019-08-05 17:45:51 -0700

    refactor API reqeust code

--- data/data.js
+++ data/data.js
@@ -1,41 +1,48 @@
 //var csv is the CSV file with headers
 export default function csvJSON(csv) {
+  var lines = csv.split("\n");
 
-    var lines = csv.split("\n");
+  var result = [];
 
-    var result = [];
+  var headers = lines[0].split(",");
 
-    var headers = lines[0].split(",");
+  for (var i = 1; i < lines.length; i++) {
+    var obj = {};
+    var currentline = lines[i].split(",");
 
-    for (var i = 1; i < lines.length; i++) {
-
-        var obj = {};
-        var currentline = lines[i].split(",");
-
-        for (var j = 0; j < headers.length; j++) {
-            obj[headers[j]] = currentline[j];
-        }
-
-        result.push(obj);
-
+    for (var j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
     }
-    let countries = {};
-    for (let k = 0; k < result.length; k++){
-        countries[result[k].Code] = result[k];
-        delete result[k]["Code"];
-        let avg = ((parseInt(result[k]["M1990"]) + parseInt(result[k]["M1995"]) + parseInt(result[k]['M2000']) + parseInt(result[k]["M2005"]) + parseInt(result[k]['M2010']) + parseInt(result[k]["M2015"])+ parseInt(result[k]['M2017']))/7/1000000).toFixed();
-        result[k]["avg"] = avg;
-        let color = "";
-        if (avg <= 1){
-            color = "LOW";
-        } else if (avg > 1 && avg <= 5){
-            color = "MEDIUM";
-        } else if ( avg > 5 && avg <= 9 ){
-            color = "HIGH";
-        } else {
-            color = "EXTREME";
-        }
-        result[k]["fillKey"] = color;
+
+    result.push(obj);
+  }
+  let countries = {};
+  for (let k = 0; k < result.length; k++) {
+    countries[result[k].Code] = result[k];
+    delete result[k]["Code"];
+    let avg = (
+      (parseInt(result[k]["M1990"]) +
+        parseInt(result[k]["M1995"]) +
+        parseInt(result[k]["M2000"]) +
+        parseInt(result[k]["M2005"]) +
+        parseInt(result[k]["M2010"]) +
+        parseInt(result[k]["M2015"]) +
+        parseInt(result[k]["M2017"])) /
+      7 /
+      1000000
+    ).toFixed();
+    result[k]["avg"] = avg;
+    let color = "";
+    if (avg <= 1) {
+      color = "LOW";
+    } else if (avg > 1 && avg <= 5) {
+      color = "MEDIUM";
+    } else if (avg > 5 && avg <= 9) {
+      color = "HIGH";
+    } else {
+      color = "EXTREME";
     }
-    return countries;
+    result[k]["fillKey"] = color;
+  }
+  return countries;
 }
--- server/api/news_api.js
+++ server/api/news_api.js
@@ -1,31 +1,33 @@
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('f90c589cb0b84e28b26d4c61173dae45');
+import newsapi from "../newsApi";
 
-const selectNews = (newsCategory) =>{
-  
-    return newsapi.v2.everything({
-        sources: 'abc-news,bbc-news,al-jazeera-english,google-news,the-new-york-times,the-verge,time,usa-today,the-washington-post',
-        q: newsCategory,
-        language: 'en',
-        sortBy: 'popularity',
-    
-        }).then(res => {
-            const articles = res.articles;
-            const ul = document.getElementById("news-ul");
-            ul.innerHTML = "";
-            for (let i = 0; i < articles.length; i++) {
-                const title = articles[i].title;
-                const url = articles[i].url;
-                const li = document.createElement('li');
-                li.innerHTML = "<a href='" + url + "'>'" + title + "'</a>" ;
-                ul.appendChild(li);
-            }
-            return articles;
-    
-        })
-        .catch(err => { 
-            console.log(err);
-        });
+// const NewsAPI = require('newsapi');
+// const newsapi = new NewsAPI('f90c589cb0b84e28b26d4c61173dae45');
+
+const selectNews = newsCategory => {
+  return newsapi.v2
+    .everything({
+      sources:
+        "abc-news,bbc-news,al-jazeera-english,google-news,the-new-york-times,the-verge,time,usa-today,the-washington-post",
+      q: newsCategory,
+      language: "en",
+      sortBy: "popularity"
+    })
+    .then(res => {
+      const articles = res.articles;
+      const ul = document.getElementById("news-ul");
+      ul.innerHTML = "";
+      for (let i = 0; i < articles.length; i++) {
+        const title = articles[i].title;
+        const url = articles[i].url;
+        const li = document.createElement("li");
+        li.innerHTML = "<a href='" + url + "'>'" + title + "'</a>";
+        ul.appendChild(li);
+      }
+      return articles;
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
-    export default selectNews;
+export default selectNews;
